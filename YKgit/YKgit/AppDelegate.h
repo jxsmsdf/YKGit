595cf1a1a1438a0a5053f6a3f6bdd367ce2fc40b
chiyan001 <1298451370@qq.com>
2016-03-04 09:44:28 +0800

    //这是第一次提交

---
+++ YKgit/YKgit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YKgit
+//
+//  Created by aaaaa on 16/3/4.
+//  Copyright © 2016年 YK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YKgit/YKgit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YKgit
+//
+//  Created by aaaaa on 16/3/4.
+//  Copyright © 2016年 YK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
